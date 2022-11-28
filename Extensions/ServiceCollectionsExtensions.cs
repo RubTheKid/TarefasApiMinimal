161a84131cbece5375536440df3446a23d80cb6f
jader.cardoso <jader.cardoso@TPCP06INF026230>
2022-11-28 11:52:52 -0300

    Add project files.

---
+++ Extensions/ServiceCollectionsExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
+using static TarefasApiMinimal.Data.TarefaContext;
+
+namespace TarefasApiMinimal.Extensions;
+
+public static class ServiceCollectionsExtensions
+{
+    public static WebApplicationBuilder AddPersistence(this WebApplicationBuilder builder)
+    {
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+        builder.Services.AddScoped<GetConnection>(sp =>
+        async () =>
+        {
+            var connection = new SqlConnection(connectionString);
+            await connection.OpenAsync();
+
+            return connection;
+        });
+        return builder;
+    }
+}
